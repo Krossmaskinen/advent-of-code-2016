ab28c23a3c4204c1839baf69d822b360a3eadb71
Bill <bill.kurtson@gmail.com>
2017-12-06 23:12:04 +0100

    day 2 - 2 complete

--- day2/main.js
+++ day2/main.js
@@ -11,12 +11,18 @@ var formattedInstructions = getFormattedInstructions(input);
 var keypad = `1 2 3
 4 5 6
 7 8 9`;
+var keypad2 = `x x 1
+x 2 3 4
+5 6 7 8 9
+x A B C
+x x D`;
 var currentPos = [1, 1];
 var code = '';
 var x = 1;
 var y = 0;
 
 keypad = getFormattedKeypad(keypad);
+keypad2 = getFormattedKeypad(keypad2);
 
 function reset() {
     currentPos = [1, 1];
@@ -38,21 +44,21 @@ function getFormattedInstructions(input) {
     return rows;
 }
 
-function followInstruction(instruction) {
+function followInstruction(instruction, keypad) {
     if (instruction === 'U') {
-        if (keypad[currentPos[y] - 1]) {
+        if (keypad[currentPos[y] - 1] && keypad[currentPos[y] - 1][currentPos[x]] && keypad[currentPos[y] - 1][currentPos[x]] !== 'x') {
             currentPos[y]--;
         }
     } else if (instruction === 'L') {
-        if (keypad[currentPos[x] - 1]) {
+        if (keypad[currentPos[y]][currentPos[x] - 1] && keypad[currentPos[y]][currentPos[x] - 1] !== 'x') {
             currentPos[x]--;
         }
     } else if (instruction === 'D') {
-        if (keypad[currentPos[y] + 1]) {
+        if (keypad[currentPos[y] + 1] && keypad[currentPos[y] + 1][currentPos[x]] && keypad[currentPos[y] + 1][currentPos[x]] !== 'x') {
             currentPos[y]++;
         }
     } else if (instruction === 'R') {
-        if (keypad[currentPos[x] + 1]) {
+        if (keypad[currentPos[y]][currentPos[x] + 1] && keypad[currentPos[y]][currentPos[x] + 1] !== 'x') {
             currentPos[x]++;
         }
     }
@@ -61,7 +67,7 @@ function followInstruction(instruction) {
 function solvePart1() {
     for (row of formattedInstructions) {
         for (instruction of row) {
-            followInstruction(instruction);
+            followInstruction(instruction, keypad);
         }
 
         code += keypad[currentPos[y]][currentPos[x]].toString();
@@ -70,4 +76,20 @@ function solvePart1() {
     console.log(`the code is ${code}`);
 }
 
+function solvePart2() {
+    currentPos = [2, 0];
+
+    for (row of formattedInstructions) {
+        for (instruction of row) {
+            followInstruction(instruction, keypad2);
+        }
+
+        code += keypad2[currentPos[y]][currentPos[x]].toString();
+    }
+
+    console.log(`the code is ${code}`);
+}
+
 solvePart1();
+reset();
+solvePart2();
